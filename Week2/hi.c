baa18266e28fc95d93b15471e6270306a518d4c9
Mihai Vaida <75952176+MihaiVaida@users.noreply.github.com>
2021-03-11 23:46:09 +0200

    Create hi.c

---
+++ Week2/hi.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // char c = '#';
+
+    // char c1 = 'H';
+    // char c2 = 'I';
+    // char c3 = '!';
+    
+    string s = "HI!";
+
+    printf("%s\n", s);
+}
