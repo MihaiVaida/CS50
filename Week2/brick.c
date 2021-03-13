bf2b32d9c220dbf70d63cadc2b9f435ef91a39a0
Mihai Vaida <75952176+MihaiVaida@users.noreply.github.com>
2021-03-13 20:31:47 +0200

    Create brick.c

---
+++ Week2/brick.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// a simple program that casts a the # symbol to the numerical representation
+
+int main(void)
+{
+    char c = '#';
+    
+    // printf("%c\n", c);
+    
+    printf("%i\n", (int) c);
+}
