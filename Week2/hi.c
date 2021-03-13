7a9e674c98981615685d4f6f5479af487c45ee7c
Mihai Vaida <75952176+MihaiVaida@users.noreply.github.com>
2021-03-13 20:21:36 +0200

    Update hi.c

--- Week2/hi.c
+++ Week2/hi.c
@@ -8,8 +8,10 @@ int main(void)
     // char c1 = 'H';
     // char c2 = 'I';
     // char c3 = '!';
-    
+
     string s = "HI!";
 
-    printf("%s\n", s);
+    // printf("%s\n", s);
+    // printf("%c %c %c\n", s[0], s[1], s[2]);
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
 }
